8f0ed050bff8e46ba5aa6655d56f8f72ddd31588
karan kumar <karanofficial1401@gmail.com>
2026-03-19 23:17:08 +0530

    Day 1: Python Basics

---
+++ Basics/day1.py
@@ -0,0 +1,15 @@
+# Day 1: Python Basics
+
+# variables
+a = 10
+b = 5
+
+print("Sum:", a + b)
+print("Multiply:", a * b)
+
+# user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old")
