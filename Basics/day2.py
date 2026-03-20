526af293522c9567e05360263d0e2956f687d545
karan kumar <karanofficial1401@gmail.com>
2026-03-20 13:14:09 +0530

    Day 2 & Day 3: Lists, Dict, Tuple Practice

---
+++ Basics/day2.py
@@ -0,0 +1,29 @@
+# python code -> byte code -> python virtual machine
+# compile to byte code where byte code is low level & platform independent 
+# byte code runs faster
+# source change and python version
+# hello_karan.cpython-312.py
+# -> works only for imported files
+# -> not for top level files
+
+# Python virtual machine
+# -> code loop to iterate byte code
+# -> run time engine 
+# -> also known as python interpretation
+# byte code is not a machine code
+# -> python specific interpretation
+
+# object types and data types 
+# -> Number: 1234, 3.1415, 4+5j, 0b111, Decimal(), Fraction()
+# -> String : 'spam', "karan"
+# -> List: [1,2,[3,'karan'],4,5], list(range(10))
+# -> tuple: (1,'spam', 4, 'u'), tuple('spam')
+# ->dict: {'Name':'karan', 'taste':'yummy'}, dict(hours=10)
+# -> Set: set('abc'), {'a','b','c'}
+
+# Boolean : true, false
+# -> None: None
+# Function , modules, classes
+#  Advance: Decorators, Generators, iterators, Meta Programming
+
+
---
+++ day3.py
@@ -0,0 +1,25 @@
+# UserName = "KaranKumar"
+# print(len(UserName))
+# print(UserName[0])
+# print(UserName[-1])
+# print(UserName[1:5])
+
+# List
+# myList = [123,"Karan", 3.14]
+# print(len(myList))
+# print(myList[0])
+# print(myList[-1])
+
+# Dict
+# myDict = {
+#     "one":"lemon",
+#     "two":"ginger",
+#     "three":"Apple"
+# }
+# print(myDict)
+# print(myDict["one"])
+
+# Tupple
+mytuple = (1,3,5,4,6);
+print(mytuple[0])
+print(mytuple)
