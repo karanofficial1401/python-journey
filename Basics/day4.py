ecd9276fdd5cce637d14183d86b68b8b1707e636
karan kumar <karanofficial1401@gmail.com>
2026-03-21 13:32:45 +0530

    Fixed structure: moved day3.py into Basics folder

---
+++ Basics/day4.py
@@ -0,0 +1,55 @@
+# a = 3
+# a = 'karan'
+# a = 3.14
+# print(a)
+# a = a+2
+# print(a)
+
+
+# Lists practice
+
+# myList = [1,2,3,4]
+# myListTwo = myList
+# myList = 'karan'
+# print(myListTwo)
+# myList = [1,2,3,4]
+# myList[0] = 33
+# print(myList)
+# print(myListTwo)
+
+
+
+# l1 = [1,2,3,4,5]
+# l2 = l1
+# print(l2)
+# # l1 = [456,48]
+# l1[0] = 10
+# print(l1)
+# print(l2)
+
+# p1 = [45,15,12,7,8]
+# p2 = p1
+# p2 = [4,5,6,7,8,9]
+# p1[0] = 55
+# print(p1)
+# print(p2)
+
+
+# h1 = [1,4,[1,25],2,4,2,4]
+# h2 = h1[0:5]
+# print(h1)
+# print(h2)
+# h1[0] = 45
+# print(h1)
+# print(h2)
+# import copy
+# h2 = copy.copy(h1)
+
+
+
+n = [1,2,3,4]
+m = n
+m = [1,2,3,4]
+print(m)
+print(n)
+print(m is n)
