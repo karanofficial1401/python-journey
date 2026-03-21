43b4557ad0b60125b864f29a7f00603083c87361
karan kumar <karanofficial1401@gmail.com>
2026-03-21 14:46:23 +0530

    Day 5: Explored numbers, operators & expressions in Python 🚀

---
+++ Basics/day5.py
@@ -0,0 +1,77 @@
+# Numbers in python
+
+x = 2
+y = 3
+z = 5
+# print(x+y)
+# print(40+12.5)
+# print(4+3)
+
+# print(x,y,z) 
+# print(x+1, y*2)
+# print(y%2)
+# print(z**5)
+# print(2**100)
+
+
+# result = 1/3.0
+# print(result)
+
+# print(repr('karan'))
+# print(str('nitin'))
+# print('rohan')
+
+# print(1<2)
+# print(5.0==5.0)
+# print(x,y,z)
+# print(x<y<z)
+# print(x<y and y<z)
+# print(1==2<3)
+# print(1==2 and 2<3)
+
+
+# import math
+# print(math.floor(3.5))
+# print(math.floor(-3.5))
+# print(math.floor(3.9))
+# print(math.trunc(-2.8)
+
+# print(9999999999999999999999999+1)
+# print(9999999999999999999999*2.1)
+
+
+# print(2+1j)
+# print((2+1j)*3)
+
+# x = 1
+# print(x<<2)
+
+
+# import random
+# print(random.random())
+# print(random.randint(1,10))
+# l1 = ['lemon', 'masala', 'ginger', 'mint']
+# print(random.choice(l1))
+# print(random.shuffle(l1))
+
+
+# from decimal import Decimal
+# print(0.1+0.1+0.1)
+# print(0.1+0.1+0.1-0.3)
+# print((0.1+0.1+0.1)-0.3)
+# print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+
+
+# setone  = {1,2,3,4}
+# print(setone & {1,2})
+# print(setone | {1,3})
+# print(setone-{1,2,3,4})
+# print(type({}))
+
+
+
+print(type(True))
+print(True == 1)
+print(False == 0)
+print(True is 1)
+print(True + 4)
